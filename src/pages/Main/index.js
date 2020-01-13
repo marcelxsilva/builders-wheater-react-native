89a171b1c4589d49772817ef5d874beedffd15f0
Marcelo Silva <maarcelomonteiro@hotmail.com>
2020-01-13 14:47:33 -0300

    created page main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -6,6 +6,7 @@ export default function Main() {
   return (
     <Container>
       <Text>Wheather</Text>
+      <Wheather />
     </Container>
   );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -3,13 +3,15 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background: #ffc107;
-  flex-direction: row;
-  justify-content: center;
+  z-index: 5;
   align-items: center;
+
 `;
 
 export const Text = styled.Text`
   font-size: 22px;
   font-weight: bold;
-  color: #FFF;
+  color: #333;
+  margin-bottom: -20px;
+  margin-top: 20px;
 `
